6a94e356bf3813e5c64003693f52c1295ba4d195
John Stockbauer <john@shippable.com>
2015-08-17 23:18:43 -0700

    menu changes

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -16,44 +16,59 @@ Meteor.startup(function () {
     };
     Groups.insert(brideAndGroom);
   }
-  if (MenuItems.find().count() === 0) {
-    var itemList = [
-      {
-        title: 'Filet medallion',
-        description: 'Filet medallion in red wine mushroom sauce',
-        type: 'beef',
-        sides: [
-          {
-            name: 'New Potatoes',
-            description: 'roasted with herbs and garlic'
-          },
-          {
-            name: 'Mixed Vegetables',
-            description: 'blend of broccoli, carrot, and green bell pepper with herbs'
-          }
-        ],
-        cost: 28
-      },
-      {
-        title: 'Salmon',
-        description: 'Salmon with lemon',
-        type: 'fish',
-        sides: [
-          {
-            name: 'New Potatoes',
-            description: 'roasted with herbs and garlic'
-          },
-          {
-            name: 'vegetables',
-            description: 'they\'re good for you'
-          }
-        ],
-        cost: 23
-      }
-    ];
+  var itemList = [
+    {
+      mainCourse: 'Filet medallion in red wine mushroom sauce',
+      type: 'beef',
+      sides: [
+        {
+          description: 'Potatoes roasted with herbs and garlic'
+        },
+        {
+          description: 'Vegetable Medley'
+        }
+      ]
+    },
+    {
+      mainCourse: 'Lemon dill salmon filet and garlic shrimp',
+      type: 'fish',
+      sides: [
+        {
+          description: 'Rice pilaf'
+        },
+        {
+          description: 'Sugar snap peas'
+        }
+      ]
+    },
+    {
+      mainCourse: 'White vegetarian lasagna',
+      type: 'vegetarian',
+      sides: [
+        {
+          description: 'Green chili smashed sweet potato'
+        },
+        {
+          description: 'Green beans'
+        }
+      ]
+    },
+    {
+      mainCourse: 'Chicken Marsala',
+      type: 'chicken',
+      sides: [
+        {
+          description: 'squash and zuchini with roma tomatoes'
+        },
+        {
+          description: 'roasted red potatoes in olive oil, basil, and garlic'
+        }
+      ]
+    }
+  ];
 
-    _.each(itemList, function (item) {
+  _.each(itemList, function (item) {
+    if (MenuItems.find({type:item.type}).count() === 0)
       MenuItems.insert(item);
-    });
-  }
+  });
 });
